ee6b52328740e458b34aeb5718b88203f258751d
Cezarr1 <87286428+Cezarr1@users.noreply.github.com>
2026-05-30 16:48:48 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("**/*.{jpg,jpeg,png,gif,webp,tif,tiff,jfif,bmp,svg,ico}");
   return {
     dir: {
